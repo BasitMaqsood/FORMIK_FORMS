a6eeee0bb320b781e8a3d66a9bef89d4b8489862
Basit <malikbasitmaqsood@gmail.com>
2020-06-30 17:44:42 +0500

    formick basics

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+// import BasicComponent from "./component/basicComponent";
+// import Basic2Component from "./component/basic2Component";
+// import SignUpComponent from "./component/signUpForm";
+// import SignUpComponent2 from "./component/FormComponent";
+import FormComponent from "./component/FormWithFormikValidation";
+
+function App() {
+  return (
+    <div>
+      <FormComponent />
+    </div>
+  );
+}
+
+export default App;
